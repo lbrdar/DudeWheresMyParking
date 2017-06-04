897efb0f5732979b452a8a46e25b0ffc5f0bfc2b
Leo Brdar <lbrdar@riteh.hr>
2017-06-04 21:41:01 +0200

    More clear export

---
+++ app/common/reducers/index.js
@@ -0,0 +1,5 @@
+export { default as drawerReducers } from './drawer';
+export { default as navigationReducers } from './navigation';
+export { default as userPositionReducers } from './userPosition';
+export { default as settingsReducers } from './settings';
+export { default as dataReducers } from './data';
