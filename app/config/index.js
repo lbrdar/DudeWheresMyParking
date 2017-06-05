aaaee70d735d62cc98eefc4e5c6577583f3a11bd
Leo Brdar <lbrdar@riteh.hr>
2017-06-06 00:14:19 +0200

    New api ip address

--- app/config/index.js
+++ app/config/index.js
@@ -1,4 +1,4 @@
 export default {
   googleAPIKey: 'AIzaSyCq80qpY1awoh2rke42iEnOu84VaaBTnn0',
-  APIserver: 'http://192.168.1.4:8080'   // needs to use IP address instead of localhost (https://github.com/facebook/react-native/issues/5222#issuecomment-198705413)
+  APIserver: 'http://192.168.1.7:8080'   // needs to use IP address instead of localhost (https://github.com/facebook/react-native/issues/5222#issuecomment-198705413)
 };
