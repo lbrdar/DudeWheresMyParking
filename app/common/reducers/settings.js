0393360dad409a28c3c7bee55079d3117c438a60
Leo Brdar <lbrdar@riteh.hr>
2017-06-08 21:12:22 +0200

    Use sharedPreferences / userDefaults for storing settings

--- app/common/reducers/settings.js
+++ app/common/reducers/settings.js
@@ -1,3 +1,4 @@
+import DefaultPreference from 'react-native-default-preference';
 import constants from '../constants';
 
 const initialState = {
@@ -8,8 +9,10 @@ const initialState = {
 export default function (state, action) {
   switch (action.type) {
     case constants.SET_MAP_TYPE:
+      DefaultPreference.set('mapType', action.mapType);
       return { ...state, mapType: action.mapType };
     case constants.SET_RADIUS:
+      DefaultPreference.set('radius', action.radius.toString());
       return { ...state, radius: action.radius };
     default:
       return state || initialState;
--- app/views/App/AppComponent.js
+++ app/views/App/AppComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DefaultPreference from 'react-native-default-preference';
 import { ThemeProvider } from 'react-native-material-ui';
 import { StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
@@ -6,9 +7,21 @@ import { myTheme } from '../../common';
 import routes from '../../routes';
 import MyDrawer from '../../views/Drawer';
 import Store from '../../common/store';
+import * as Actions from '../../common/actions/settings';
 
 
 const AppNavigator = StackNavigator(routes);
+
+// get user preferences
+DefaultPreference
+  .getMultiple(['radius', 'mapType'])
+  .then((res) => {
+    const radius = (res[0] !== null) ? parseInt(res[0]) || 300 : 300;
+    const mapType = (res[1] !== null) ? res[1] : 'hybrid';
+
+    Store.dispatch(Actions.setRadius(radius));
+    Store.dispatch(Actions.setMapType(mapType));
+  });
 
 class App extends Component {  // eslint-disable-line react/prefer-stateless-function
   render() {
