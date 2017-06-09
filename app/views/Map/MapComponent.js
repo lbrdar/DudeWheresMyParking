7f8ecb74b72318c5ed33b9504c6aa479d1b5e4b6
Leo Brdar <lbrdar@riteh.hr>
2017-06-09 10:33:25 +0200

    Add marker colors

--- app/views/Map/MapComponent.js
+++ app/views/Map/MapComponent.js
@@ -8,6 +8,10 @@ import { connect } from 'react-redux';
 import { geoLocationUtils, API } from '../../utils';
 import { Loading } from '../../common';
 import MapHeader from './MapHeader';
+import markerRed from '../../images/marker-red.png';
+import markerYellow from '../../images/marker-yellow.png';
+import markerGreen from '../../images/marker-green.png';
+import markerGray from '../../images/marker-gray.png';
 import styles from './MapStyles';
 import * as Actions from '../../common/actions';
 
@@ -123,6 +127,19 @@ class Map extends React.Component {
     });
   };
 
+  determineMarkerColor = (created, isTaken) => {
+    const deltaMs = moment() - moment(created);
+    if (isTaken) {
+      return markerGray
+    } else if (deltaMs < 15*60*1000) { // 15 min
+      return markerGreen;
+    } else if (deltaMs < 30*60*1000) { // 30 min
+      return markerYellow;
+    } else {
+      return markerRed;
+    }
+  };
+
   renderMarkerCallout = () => {
     const { type, cost, taken, takenFor } = this.state.selectedParkingSpot;
     return (
@@ -135,11 +152,15 @@ class Map extends React.Component {
       </View>
     );
   };
-  renderMarker = ({ id, latitude, longitude }) => {
+  renderMarker = ({ id, latitude, longitude, created, taken, takenFor }) => {
     const { selectedParkingSpot, activeMarker } = this.state;
+
+    if (taken && moment(taken).add(takenFor, 'seconds') > moment()) return null; // not free yet, no need to show it to user; TODO: filter it on API
+
     return (
       <MapView.Marker
         key={id}
+        image={this.determineMarkerColor(created, !!taken)}
         coordinate={{ latitude, longitude }}
         onPress={() => this.onMarkerPress(id)}
       >
